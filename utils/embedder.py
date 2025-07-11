391e646e6dc4e7c8848da5ee3d7841d835ec9bbb
ssimkani <seenasimkani@gmail.com>
2025-07-10 22:37:15 -0400

    Uncache embedder

--- utils/embedder.py
+++ utils/embedder.py
@@ -2,7 +2,6 @@ import google.generativeai as genai
 from utils.config import EMBEDDING_MODEL
 import streamlit as st
 
-@st.cache_data(ttl=600)
 def get_gemini_embedding(text: str) -> list[float]:
     res = genai.embed_content(
         model=EMBEDDING_MODEL, content=text, task_type="RETRIEVAL_QUERY"
