7b8b9729621cb9f6b8381d5efc0f1a9f5baec22e
ssimkani <seenasimkani@gmail.com>
2025-07-14 14:27:07 -0400

    Add streaming from gemini

--- utils/chat_helper.py
+++ utils/chat_helper.py
@@ -6,19 +6,27 @@ import time
 llm = genai.GenerativeModel(MODEL_NAME)
 
 def generate_response(prompt: str, temperature) -> str:
-
-    response_container = st.empty()
     response = llm.generate_content(prompt,
-        generation_config=genai.GenerationConfig(
-            temperature=temperature,
-            stream=True
-                                    ))
-    response_text = response.text.strip()
+        generation_config=genai.GenerationConfig(temperature=temperature),
+        stream=True)
+    # response_text = response.text.strip()
+
+    # # streaming output
+    # with response_container:
+    #     # for i in range(1, len(response_text) + 1):
+    #     #     st.markdown(response_text[:i] + "▌")
+    #     #     time.sleep(0.001)
+    #     for chunk in response:
+    #         st.markdown(chunk.text)
+    # return response_text
+    
+    response_container = st.empty()
+    full_response = ""
 
     # streaming output
-    with response_container:
-        # for i in range(1, len(response_text) + 1):
-        #     st.markdown(response_text[:i] + "▌")
-        #     time.sleep(0.001)
-        st.markdown(response_text)
-    return response_text
+    for chunk in response:
+        full_response += chunk.text
+        response_container.markdown(full_response + "▌")
+
+    response_container.markdown(full_response)
+    return full_response
